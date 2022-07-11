ef675a8c105e90a26da99cd87cc4aeca03747cf7
cornel.plamadeala <cornel.plamadeala@gmail.com>
2022-07-11 13:44:21 +0300

    Add paynet md skeleton

---
+++ src/PaynetCode.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Paymentmd\Paynet;
+
+class PaynetCode
+{
+    const SUCCESS = 0;
+    const TECHNICAL_ERROR = 1;
+    const DATABASE_ERROR = 2;
+    const USERNAME_OR_PASSWORD_WRONG = 3;
+    const CONNECTION_ERROR = 12;
+}
---
+++ src/PaynetResult.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Paymentmd\Paynet;
+
+class PaynetResult
+{
+    public $Code ;
+    public $Message ;
+    public $Data ;
+    public function IsOk()
+    {
+        return $this->Code === PaynetCode::SUCCESS;
+    }
+}
